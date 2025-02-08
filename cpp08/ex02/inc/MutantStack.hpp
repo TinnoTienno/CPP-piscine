93bdc1e6d2400f4abe304fe73ee1507f359905af
noda-debian <etienne.schussler21@gmail.com>
2025-02-08 19:14:45 +0100

    cpp 08-end

--- cpp08/ex02/inc/MutantStack.hpp
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -3,29 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eschussl <eschussl@student.42.fr>          +#+  +:+       +#+        */
+/*   By: noda <noda@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:58:13 by eschussl          #+#    #+#             */
-/*   Updated: 2024/11/19 12:58:23 by eschussl         ###   ########.fr       */
+/*   Updated: 2025/02/08 19:07:23 by noda             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 #include <stack>
+#include <deque>
 
-template <class T>
-class MutantStack : public std::stack<T>
-{
-	typedef std::stack<T> Stack;
-	using Stack::c;
-	public :
-		using Stack::operator=;
-		using Stack::size;
-    	using Stack::push;
-    	using Stack::pop;
-    	using Stack::top;
-		using Stack::iterator;
-}	;
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+public:
+    // Définition explicite des itérateurs sans using
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
+};
 
 #endif
